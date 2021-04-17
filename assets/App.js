b44f5af1acd2f3581e4c485da7e33b8c7dcf67f4
Justyna Ilczuk <justyna.ilczuk@gmail.com>
2021-04-17 15:19:24 +0100

    Integrate React into the app.

---
+++ assets/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello from React!</h1>
+                <p>Great success!</p>
+            </div>
+        );
+    }
+}
--- invertimo/views.py
+++ invertimo/views.py
@@ -5,21 +5,21 @@ from django.http import HttpResponse
 
 
 def index_view(request):
-    if not  request.user.is_authenticated:
-        return render(request, 'landing.html', {})
-    return render(request, 'index.html', {})
+    if not request.user.is_authenticated:
+        return render(request, "landing.html", {})
+    return render(request, "index.html", {})
 
 
 def login_view(request):
     context = {}
-    return render(request, 'login.html', context)
+    return render(request, "login.html", context)
 
 
 def privacy_policy_view(request):
     context = {}
-    return render(request, 'privacy_policy.html', context)
+    return render(request, "privacy_policy.html", context)
 
 
 def logout_view(request):
     logout(request)
-    return redirect('/')
+    return redirect("/")
