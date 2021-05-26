b00a5b77ef13b7eef11e4f7c2a18c47075f1e80a
Justyna Ilczuk <justyna.ilczuk@gmail.com>
2021-05-26 21:58:47 +0100

    Migrate db to postgresql

--- invertimo/settings.py
+++ invertimo/settings.py
@@ -82,9 +82,13 @@ WSGI_APPLICATION = "invertimo.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'invertimo',
+        'USER': 'myuser',
+        'PASSWORD': DB_KEY,
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
