7d389a58fc427f1feb8e0e98d250f81ed6b43276
Justyna Ilczuk <justyna.ilczuk@gmail.com>
2021-06-06 19:08:43 +0100

    Handle multiple accounts in the overview section
    - even if they use different currencies and so on, they should display
    correctly
    - added simple snapshot tests

--- assets/Portfolio.js
+++ assets/Portfolio.js
@@ -4,6 +4,7 @@ import './portfolio.css';
 import PositionList from './PositionList.js';
 import TimeSelector from './TimeSelector.js';
 import { APIClient } from './api_utils.js';
+import PropTypes from 'prop-types';
 
 
 const CURRENCY_TO_SYMBOL = new Map(
@@ -14,30 +15,12 @@ const CURRENCY_TO_SYMBOL = new Map(
     ]
 )
 
-
-class PortfolioOverview extends React.Component {
-
+class AccountStats extends React.Component {
     render() {
-        let positions_count = 0;
-        let transactions_count = 0;
-        let account_event_count = "?";
-        let total_value = 0;
+        const account = this.props.account;
 
-        let accounts = this.props.accounts;
-        for (let account of accounts) {
-            positions_count += account.positions_count;
-            transactions_count += account.transactions_count;
-            total_value += Number(account.balance);
-        }
-        let account_count = accounts.length;
-
-        let currencySymbol = "?";
-        if (accounts.length) {
-            currencySymbol = CURRENCY_TO_SYMBOL.has(accounts[0].currency) ? CURRENCY_TO_SYMBOL.get(accounts[0].currency) : accounts[0].currency;
-        }
-        // TODO: rethink if positions should have an api endpoint per account or not
-        // (likely, yes, because accounts can be in different currencies).
-        // Assumes all accounts in one currency for now.
+        let totalCash = Number(account.balance);
+        let currencySymbol = CURRENCY_TO_SYMBOL.has(account.currency) ? CURRENCY_TO_SYMBOL.get(account.currency) : account.currency;
 
         const assetValues = this.props.positions.map(position => {
             // Exchange rate is null if account and position trading currency are the same.
@@ -48,39 +31,98 @@ class PortfolioOverview extends React.Component {
             assetValues.reduce((sum, current) => sum + current, 0) * 100) / 100;
 
         return (
+            <ul className="portfolio-stats-list">
+                <li className="portfolio-stats-account-name">
+                    In <a href="">{account.nickname}</a> account:
+                </li>
+                <li>
+                    Total Cash: {totalCash} {currencySymbol}
+                </li>
+                <li>
+                    Total Assets: {totalAssetValue} {currencySymbol}
+                </li>
+                <li>
+                    Portfolio Value: {Math.round((totalCash + totalAssetValue) * 100) / 100} {currencySymbol}
+                </li>
+            </ul>);
+    }
+}
+
+
+AccountStats.propTypes = {
+    positions: PropTypes.array.isRequired,
+    account: PropTypes.object.isRequired,
+}
+
+export function divideByAccount(accounts, positions) {
+    const positionsByAccount = [];
+    for (let account of accounts) {
+        const positionsForAccount = [];
+        for (let position of positions) {
+            if (position.account == account.id) {
+                positionsForAccount.push(position);
+            }
+        }
+        positionsByAccount.push(
+            { account: account, positions: positionsForAccount }
+        );
+    }
+    return positionsByAccount;
+}
+
+
+export class PortfolioOverview extends React.Component {
+
+    render() {
+        let positionsCount = 0;
+        let transactionsCount = 0;
+        let accountEventCount = "?";
+
+        let accounts = this.props.accounts;
+
+        const accountStatsEntries = divideByAccount(accounts, this.props.positions).map(entry => {
+            return (
+                <AccountStats
+                    key={entry.account.id}
+                    account={entry.account}
+                    positions={entry.positions}
+                />
+            )
+        })
+        for (let account of accounts) {
+            positionsCount += account.positions_count;
+            transactionsCount += account.transactions_count;
+        }
+
+        return (
             <div className="portfolio-overview">
                 <div className="portfolio-overview-card card">
                     <span className="card-label">At a glance</span>
-                    <ul className="portfolio-stats-list">
-                        <li>
-                            Total Cash: {total_value} {currencySymbol}
-                        </li>
-                        <li>
-                            Total Assets: {totalAssetValue} {currencySymbol}
-                        </li>
-                        <li>
-                            Portfolio Value: {Math.round((total_value + totalAssetValue) * 100) / 100} {currencySymbol}
-                        </li>
-                    </ul>
+                    {accountStatsEntries}
                 </div>
                 <div className="card">
                     <span className="card-label">Assets</span>
                     <p>
-                        {positions_count} <a href="">Positions</a> in {account_count}  <a href=""> {account_count > 1 ? "Accounts" : "Account"}</a>
+                        {positionsCount} <a href="">Positions</a> in {accounts.length}  <a href=""> {accounts.length > 1 ? "Accounts" : "Account"}</a>
                     </p>
                     <a className="button">See all Positions</a>
                     <a className="button">Manage accounts</a>
                 </div>
                 <div className="card">
                     <span className="card-label">Events</span>
-                    <div>{transactions_count} <a href="">Transactions</a></div>
-                    <div>{account_event_count} <a href="">Account Events</a></div>
+                    <div>{transactionsCount} <a href="">Transactions</a></div>
+                    <div>{accountEventCount} <a href="">Account Events</a></div>
                     <a className="button">Manage transactions</a>
                     <a className="button">Manage events</a>
                 </div>
             </div>
         )
     }
+}
+
+PortfolioOverview.propTypes = {
+    positions: PropTypes.array.isRequired,
+    accounts: PropTypes.array.isRequired,
 }
 
 class PortfolioChart extends React.Component {
--- assets/Portfolio.test.js
+++ assets/Portfolio.test.js
@@ -5,7 +5,8 @@ import pretty from "pretty";
 
 // The import below is necessary for async/await to work.
 import regeneratorRuntime from "regenerator-runtime";
-import Portfolio from "./Portfolio";
+import Portfolio from "./Portfolio.js";
+import { PortfolioOverview, divideByAccount } from "./Portfolio.js";
 
 let container = null;
 beforeEach(() => {
@@ -21,6 +22,112 @@ afterEach(() => {
   container = null;
 });
 
+const _ACCOUNT_POSITIONS = [
+  {
+    id: 91,
+    account: 3,
+    security: {
+      id: 1,
+      isin: "US2546871060",
+      symbol: "DIS",
+      name: "The Walt Disney Company",
+      exchange: {
+        id: 132,
+        name: "USA Stocks",
+      },
+      currency: "USD",
+      country: "USA",
+    },
+    quantity: "35.00",
+    latest_price: "176.24",
+    latest_price_date: "2021-06-03",
+    latest_exchange_rate: "0.82470000",
+  },
+  {
+    id: 93,
+    account: 3,
+    security: {
+      id: 3,
+      isin: "US4781601046",
+      symbol: "JNJ",
+      name: "Johnson & Johnson",
+      exchange: {
+        id: 132,
+        name: "USA Stocks",
+      },
+      currency: "USD",
+      country: "USA",
+    },
+    quantity: "29.00",
+    latest_price: "166.04",
+    latest_price_date: "2021-06-03",
+    latest_exchange_rate: "0.82470000",
+  },
+  {
+    id: 96,
+    account: 3,
+    security: {
+      id: 6,
+      isin: "IE00BF4RFH31",
+      symbol: "IUSN",
+      name: "iShares MSCI World Small Cap UCITS ETF USD (Acc)",
+      exchange: {
+        id: 143,
+        name: "XETRA Exchange",
+      },
+      currency: "EUR",
+      country: "Germany",
+    },
+    quantity: "2775.00",
+    latest_price: "6.06",
+    latest_price_date: "2021-06-04",
+    latest_exchange_rate: null,
+  },
+];
+
+const _ACCOUNT_POSITIONS_USD = [
+  {
+    id: 121,
+    account: 4,
+    security: {
+      id: 1,
+      isin: "US2546871060",
+      symbol: "DIS",
+      name: "The Walt Disney Company",
+      exchange: {
+        id: 132,
+        name: "USA Stocks",
+      },
+      currency: "USD",
+      country: "USA",
+    },
+    quantity: "16.00",
+    latest_price: "176.24",
+    latest_price_date: "2021-06-03",
+    latest_exchange_rate: null,
+  },
+  {
+    id: 122,
+    account: 4,
+    security: {
+      id: 2,
+      isin: "US0378331005",
+      symbol: "AAPL",
+      name: "Apple Inc",
+      exchange: {
+        id: 132,
+        name: "USA Stocks",
+      },
+      currency: "USD",
+      country: "USA",
+    },
+    quantity: "18.00",
+    latest_price: "123.54",
+    latest_price_date: "2021-06-03",
+    latest_exchange_rate: null,
+  },
+];
+
 jest.mock("./api_utils", () => {
   // Works and lets you check for constructor calls:
   return {
@@ -29,7 +136,7 @@ jest.mock("./api_utils", () => {
         getAccounts: () => {
           let accounts = [
             {
-              id: 2,
+              id: 3,
               currency: "EUR",
               nickname: "test account",
               description: "",
@@ -42,13 +149,171 @@ jest.mock("./api_utils", () => {
           return Promise.resolve(accounts);
         },
         getPositions: () => {
-          let positions = [];
-          return Promise.resolve(positions);
+          return Promise.resolve(_ACCOUNT_POSITIONS);
         },
         getPositionDetail: (positionId) => {},
       };
     }),
   };
+});
+
+it("shows portfolio overview with one account", async () => {
+  const accounts = [
+    {
+      id: 3,
+      currency: "EUR",
+      nickname: "test account",
+      description: "",
+      balance: "-16584.76000",
+      last_modified: "2021-05-03T14:20:29.732353Z",
+      positions_count: 3,
+      transactions_count: 15,
+    },
+  ];
+  const positions = _ACCOUNT_POSITIONS;
+  await act(async () => {
+    render(
+      <PortfolioOverview accounts={accounts} positions={positions} />,
+      container
+    );
+  });
+
+  expect(pretty(container.innerHTML)).toMatchInlineSnapshot(`
+    "<div class=\\"portfolio-overview\\">
+      <div class=\\"portfolio-overview-card card\\"><span class=\\"card-label\\">At a glance</span>
+        <ul class=\\"portfolio-stats-list\\">
+          <li class=\\"portfolio-stats-account-name\\">In <a href=\\"\\">test account</a> account:</li>
+          <li>Total Cash: -16584.76 €</li>
+          <li>Total Assets: 25874.64 €</li>
+          <li>Portfolio Value: 9289.88 €</li>
+        </ul>
+      </div>
+      <div class=\\"card\\"><span class=\\"card-label\\">Assets</span>
+        <p>3 <a href=\\"\\">Positions</a> in 1 <a href=\\"\\"> Account</a></p><a class=\\"button\\">See all Positions</a><a class=\\"button\\">Manage accounts</a>
+      </div>
+      <div class=\\"card\\"><span class=\\"card-label\\">Events</span>
+        <div>15 <a href=\\"\\">Transactions</a></div>
+        <div>? <a href=\\"\\">Account Events</a></div><a class=\\"button\\">Manage transactions</a><a class=\\"button\\">Manage events</a>
+      </div>
+    </div>"
+  `);
+});
+
+it("divides positions by account correctly", () => {
+  const accounts = [
+    {
+      id: 3,
+      currency: "EUR",
+      nickname: "test account",
+      description: "",
+      balance: "-16584.76000",
+      last_modified: "2021-05-03T14:20:29.732353Z",
+      positions_count: 3,
+      transactions_count: 15,
+    },
+    {
+      id: 4,
+      currency: "USD",
+      nickname: "second account",
+      description: "",
+      balance: "161",
+      last_modified: "2021-05-03T14:20:29.732353Z",
+      positions_count: 2,
+      transactions_count: 5,
+    },
+  ];
+
+  const positions = _ACCOUNT_POSITIONS.concat(_ACCOUNT_POSITIONS_USD);
+  const positionsByAccount = divideByAccount(accounts, positions);
+  expect(positionsByAccount).toHaveLength(2);
+  expect(positionsByAccount[0].account.nickname).toBe("test account");
+  expect(positionsByAccount[0].positions).toHaveLength(3);
+  expect(positionsByAccount[1].account.nickname).toBe("second account");
+  expect(positionsByAccount[1].positions).toHaveLength(2);
+});
+
+it("shows portfolio overview with two accounts", async () => {
+  const accounts = [
+    {
+      id: 3,
+      currency: "EUR",
+      nickname: "test account",
+      description: "",
+      balance: "-16584.76000",
+      last_modified: "2021-05-03T14:20:29.732353Z",
+      positions_count: 3,
+      transactions_count: 15,
+    },
+    {
+      id: 4,
+      currency: "USD",
+      nickname: "second account",
+      description: "",
+      balance: "161",
+      last_modified: "2021-05-03T14:20:29.732353Z",
+      positions_count: 2,
+      transactions_count: 5,
+    },
+  ];
+
+  const positions = _ACCOUNT_POSITIONS.concat(_ACCOUNT_POSITIONS_USD);
+  await act(async () => {
+    render(
+      <PortfolioOverview accounts={accounts} positions={positions} />,
+      container
+    );
+  });
+
+  expect(pretty(container.innerHTML)).toMatchInlineSnapshot(`
+    "<div class=\\"portfolio-overview\\">
+      <div class=\\"portfolio-overview-card card\\"><span class=\\"card-label\\">At a glance</span>
+        <ul class=\\"portfolio-stats-list\\">
+          <li class=\\"portfolio-stats-account-name\\">In <a href=\\"\\">test account</a> account:</li>
+          <li>Total Cash: -16584.76 €</li>
+          <li>Total Assets: 25874.64 €</li>
+          <li>Portfolio Value: 9289.88 €</li>
+        </ul>
+        <ul class=\\"portfolio-stats-list\\">
+          <li class=\\"portfolio-stats-account-name\\">In <a href=\\"\\">second account</a> account:</li>
+          <li>Total Cash: 161 $</li>
+          <li>Total Assets: 5043.56 $</li>
+          <li>Portfolio Value: 5204.56 $</li>
+        </ul>
+      </div>
+      <div class=\\"card\\"><span class=\\"card-label\\">Assets</span>
+        <p>5 <a href=\\"\\">Positions</a> in 2 <a href=\\"\\"> Accounts</a></p><a class=\\"button\\">See all Positions</a><a class=\\"button\\">Manage accounts</a>
+      </div>
+      <div class=\\"card\\"><span class=\\"card-label\\">Events</span>
+        <div>20 <a href=\\"\\">Transactions</a></div>
+        <div>? <a href=\\"\\">Account Events</a></div><a class=\\"button\\">Manage transactions</a><a class=\\"button\\">Manage events</a>
+      </div>
+    </div>"
+  `);
+});
+
+it("shows portfolio overview without any accounts", async () => {
+  const accounts = [];
+
+  const positions = [];
+  await act(async () => {
+    render(
+      <PortfolioOverview accounts={accounts} positions={positions} />,
+      container
+    );
+  });
+
+  expect(pretty(container.innerHTML)).toMatchInlineSnapshot(`
+    "<div class=\\"portfolio-overview\\">
+      <div class=\\"portfolio-overview-card card\\"><span class=\\"card-label\\">At a glance</span></div>
+      <div class=\\"card\\"><span class=\\"card-label\\">Assets</span>
+        <p>0 <a href=\\"\\">Positions</a> in 0 <a href=\\"\\"> Account</a></p><a class=\\"button\\">See all Positions</a><a class=\\"button\\">Manage accounts</a>
+      </div>
+      <div class=\\"card\\"><span class=\\"card-label\\">Events</span>
+        <div>0 <a href=\\"\\">Transactions</a></div>
+        <div>? <a href=\\"\\">Account Events</a></div><a class=\\"button\\">Manage transactions</a><a class=\\"button\\">Manage events</a>
+      </div>
+    </div>"
+  `);
 });
 
 it("renders portfolio value", async () => {
@@ -61,12 +326,10 @@ it("renders portfolio value", async () => {
       <div class=\\"portfolio-overview\\">
         <div class=\\"portfolio-overview-card card\\"><span class=\\"card-label\\">At a glance</span>
           <ul class=\\"portfolio-stats-list\\">
-            <li>Total Value: -106584.76 €</li>
-            <li>1 Week Δ: +12.24 €</li>
-            <li>1 Month Δ: -145.24 €</li>
-            <li>3 Months Δ: +15.24 €</li>
-            <li>6 Months Δ: +123.24 €</li>
-            <li>1 Year Δ: +1245.24 €</li>
+            <li class=\\"portfolio-stats-account-name\\">In <a href=\\"\\">test account</a> account:</li>
+            <li>Total Cash: -106584.76 €</li>
+            <li>Total Assets: 25874.64 €</li>
+            <li>Portfolio Value: -80710.12 €</li>
           </ul>
         </div>
         <div class=\\"card\\"><span class=\\"card-label\\">Assets</span>
@@ -88,6 +351,33 @@ it("renders portfolio value", async () => {
               <li>Price</li>
               <li>Value</li>
             </ul>
+          </li>
+          <li>
+            <div class=\\"position-card \\">
+              <div class=\\"position-name\\"><span class=\\"card-label\\">US2546871060</span><span class=\\"position-symbol\\">DIS</span><span>The Walt Disney Company</span></div>
+              <div>USA Stocks</div>
+              <div>35.00</div>
+              <div><span class=\\"card-label\\">As of 2021-06-03</span>176.24 USD</div>
+              <div><span>6168.4 USD</span><span>5087.08 EUR</span></div>
+            </div>
+          </li>
+          <li>
+            <div class=\\"position-card \\">
+              <div class=\\"position-name\\"><span class=\\"card-label\\">US4781601046</span><span class=\\"position-symbol\\">JNJ</span><span>Johnson &amp; Johnson</span></div>
+              <div>USA Stocks</div>
+              <div>29.00</div>
+              <div><span class=\\"card-label\\">As of 2021-06-03</span>166.04 USD</div>
+              <div><span>4815.16 USD</span><span>3971.06 EUR</span></div>
+            </div>
+          </li>
+          <li>
+            <div class=\\"position-card \\">
+              <div class=\\"position-name\\"><span class=\\"card-label\\">IE00BF4RFH31</span><span class=\\"position-symbol\\">IUSN</span><span>iShares MSCI World Small Cap UCITS ETF USD (Acc)</span></div>
+              <div>XETRA Exchange</div>
+              <div>2775.00</div>
+              <div><span class=\\"card-label\\">As of 2021-06-04</span>6.06 EUR</div>
+              <div><span>16816.5 EUR</span><span></span></div>
+            </div>
           </li>
         </ul>
       </div>
