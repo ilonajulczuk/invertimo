56b680f3a094007126f59f6f1a35bfff0cba4ab0
Justyna Ilczuk <justyna.ilczuk@gmail.com>
2021-11-24 22:05:45 +0000

    Set up cors

--- invertimo/settings.py
+++ invertimo/settings.py
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
     "finance.apps.FinanceConfig",
     "rest_framework",
     "social_django",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -198,6 +200,12 @@ LOGGING = {
     },
 }
 
+
+CORS_ALLOWED_ORIGINS = [
+    f"https://{SITENAME}",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000"
+]
 
 # Other
 
