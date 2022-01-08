003b8c2d2817f9c17447b09fde9a4be563c737cc
Justyna Ilczuk <justyna.ilczuk@gmail.com>
2022-01-08 21:25:43 +0000

    Fix issue with double-loading the bundle

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   ],
   entry: './assets/index.js',  // path to our input file
   output: {
-    filename: '[contenthash].index-bundle.js',  // output bundle file name
+    filename: '[name].[contenthash].index-bundle.js',  // output bundle file name
     path: path.resolve(__dirname, './static'),  // path to our Django static directory
   },
   devtool: 'eval-source-map',
