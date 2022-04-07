88aa069f2b9461f334478536613c061078993be5
Justyna Ilczuk <justyna.ilczuk@gmail.com>
2022-04-07 22:22:45 +0100

    Try if not sending PII fixes sentry

--- invertimo/settings.py
+++ invertimo/settings.py
@@ -245,5 +245,5 @@ if SENTRY_DSN:
 
         # If you wish to associate users to errors (assuming you are using
         # django.contrib.auth) you may enable sending PII data.
-        send_default_pii=True
+        send_default_pii=False
     )
