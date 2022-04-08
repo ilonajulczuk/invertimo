501982757e7c3b6bd243912b5acf7820446ffe0c
Justyna Ilczuk <justyna.ilczuk@gmail.com>
2022-04-08 10:01:37 +0100

    Extend gunicorn timeout to avoid exiting on slow requests

--- deployment/app/docker_entrypoint.sh
+++ deployment/app/docker_entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /usr/src/venv/bin/python3.8 manage.py migrate --noinput
 /usr/src/venv/bin/python3.8 manage.py collectstatic --noinput
-/usr/src/venv/bin/gunicorn -b 0.0.0.0:8000 --workers 4 invertimo.wsgi
+/usr/src/venv/bin/gunicorn -b 0.0.0.0:8000 --workers 4 --timeout 300 invertimo.wsgi
